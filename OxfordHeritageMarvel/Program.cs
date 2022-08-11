f46c9f0bb5a158aba23009e6fa9dcabae7dbb979
Dan6ladi <dan6ladi@gmail.com>
2022-08-11 19:37:20 +0100

    Add project files.

---
+++ OxfordHeritageMarvel/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OxfordHeritageMarvel
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var payment = new List<Payment>();
+            payment.Add(new Agrolyfe() { interest = 0.22d, AgencyFee = 0.1d });
+            payment.Add(new Buy2Sell() { interest = 0.22d, AgencyFee = 0.1d });
+
+            Returns returns = new Returns();
+            returns.TotalClientPay(payment);
+            
+         
+
+          
+
+
+            Console.WriteLine("Welcome to OxfordHeritageMarvel.\n please enter your firstname and last name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("please enter amount you want to deposit");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter duration");
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n Your Returns for the duration is: " + (returns.TotalClientPay(payment)* amount));
+        }
+    }
+}
---
+++ OxfordHeritageMarvel/SrpNOp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxfordHeritageMarvel
+{
+    public abstract class Payment 
+    {
+        public double AgencyFee = 0.1d;
+        public double interest = 0.22d;
+        public double deposit { get; set; }
+
+        public abstract double CalcReturns();
+    }
+    public class Returns
+    {
+        public double TotalClientPay(List<Payment> investments) 
+        {
+
+            double totalReturns = 0;
+
+            try
+            {
+                foreach (var payment in investments)
+                {
+                    totalReturns = payment.CalcReturns();
+
+                }
+                Console.WriteLine(totalReturns);
+            }
+            catch (Exception message)
+            {
+
+                throw message;
+            }
+            return totalReturns;
+            
+        }
+
+    }
+    public class Agrolyfe : Payment
+    {
+        public double deposit { get; set; }
+        public double interest { get; set; }
+        public double AgencyFee = 0.1d;
+
+        public override double CalcReturns()
+        {
+            return deposit * interest * AgencyFee;
+        }
+    }
+    public class Buy2Sell : Payment
+    {
+        public double deposit { get; set; }
+        public double interest { get; set; }
+        public double AgencyFee = 0.1d;
+
+        public override double CalcReturns()
+        {
+            return deposit * interest * AgencyFee;
+        }
+    }
+}
